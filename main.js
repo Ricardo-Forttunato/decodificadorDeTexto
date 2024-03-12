45a9b7952a72a39bdf0cbd504e961f1de5eefff4
João Ricardo <ricardo.forttunato@gmail.com>
2024-03-12 15:11:43 -0300

    refatorar algoritmo do arquivo main.js e melhora do layout da pagina

--- main.js
+++ main.js
@@ -5,41 +5,24 @@ let message_decoded;
 document.querySelector(".btn-encrypt").addEventListener("click", function () {
     message = document.getElementById("input_area").value;
     let codedMessage = document.querySelector("#coded__message").innerHTML = decoder_text(message);
-
-    document.querySelector("#error_message").setAttribute("hidden", "hidden");
-    document.querySelector(".text-decoder--main__out-img").setAttribute("hidden", "hidden");
-    document.querySelector(".btn-copy").removeAttribute("hidden");
+    resetPage();
 })
-
-document.querySelector(".btn-decrypt").addEventListener("click", function(){
-    message = document.getElementById("input_area").value;
-    let codedMessage = document.querySelector("#coded__message").innerHTML = message;
-    console.log(message);
-});
 
 document.querySelector(".btn-decrypt").addEventListener("click", function(){
     message = document.getElementById("input_area").value;
     message_decoded = descripted(message);
     document.querySelector("#coded__message").innerHTML = message_decoded;
-    document.querySelector(".btn-copy").removeAttribute("hidden");
-    document.querySelector(".text-decoder--main__out-img").setAttribute("hidden", "hidden");
-
+    resetPage();
 });
 
 document.querySelector(".btn-copy").addEventListener("click", function(){
     copyTextToClipboard(document.querySelector("#coded__message").innerHTML);
-    document.querySelector(".btn-copy").removeAttribute("hidden");
-});
-
-// reset web page to original without using the refresh button
-document.querySelector(".btn-copy").addEventListener("click", function(){
     document.getElementById("input_area").value = "";
     document.getElementById("coded__message").innerHTML = "Nenhuma mensagem encontrada";
     document.querySelector(".btn-copy").setAttribute("hidden", "hidden");
     document.querySelector("#error_message").removeAttribute("hidden");
     document.querySelector(".text-decoder--main__out-img").removeAttribute("hidden");
 });
-
 
 // A function that takes a string as an argument and returns a decoded version of the string.
 function decoder_text(text){
@@ -68,6 +51,7 @@ function decoder_text(text){
     return letters
 };
 
+// function that takes a string as an argument and returns a undecoded version of the string.
 function descripted(text){
     text = text.replace(/ai/g, "a");
     text = text.replace(/enter/g, "e");
@@ -77,10 +61,18 @@ function descripted(text){
     return text;
 }
 
+// function that takes a string as an argument and returns a copy of the string to the clipboard.
 function copyTextToClipboard(text) {
     navigator.clipboard.writeText(text).then(function() {
-        alert("Texto copiado para a área de transferência " + text);
+        alert("Texto copiado para a área de transferência \n", text);
     }, function(err) {
         console.error('Async: Could not copy text: ', err);
     });
+  }
+
+// function that resets the page to its original state.
+function resetPage(){
+    document.querySelector("#error_message").setAttribute("hidden", "hidden");
+    document.querySelector(".text-decoder--main__out-img").setAttribute("hidden", "hidden");
+    document.querySelector(".btn-copy").removeAttribute("hidden");
   }
